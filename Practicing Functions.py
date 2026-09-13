260b03b8911f6cac596af43d532090cb32e6e470
pmadondo <patriciapmadondo@gmail.com>
2026-09-13 21:43:15 +0200

    Initial commit

---
+++ Practicing Functions.py
@@ -0,0 +1,20 @@
+
+def happy_birthday(name, age):
+    print(f'Happy birthday {name}')
+    print(f"You are old {age}")
+
+happy_birthday("Tahira", 45)
+happy_birthday('Bush', 60)
+happy_birthday('Toshni', 45)
+
+#Invoicing
+
+def your_invoice (name, amount, date):
+    print(f'Hello {name}')
+    print(f'You have ${amount} due')
+    print(f'on {date}')
+
+your_invoice('Markos', 34, 12/4 )
+your_invoice('Patricia',44,3/4)
+your_invoice('Anesu', 66, 11/11)
+
